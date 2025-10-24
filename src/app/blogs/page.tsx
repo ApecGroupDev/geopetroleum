61cf2a10b34c5311c5e458acbae86327c419b5c1
Mark Balajadia <markb.freelance@gmail.com>
2025-10-25 04:41:45 +0800

    blogs page.tsx update

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -3,6 +3,11 @@ import Header from "@/components/header";
 import { Metadata } from "next";
 import CTASection from "@/components/cta";
 import ServicesSchema from "@/components/schema/ServicesSchema";
+import FAQ_Blogs from "./sections/faq_blogs";
+import Hero_Blogs from "./sections/hero_blogs";
+import Intro_Blogs from "./sections/intro_blogs";
+import Topics_Blogs from "./sections/topics_blogs";
+import Audience_Blogs from "./sections/audience_blogs";
 
 export const metadata: Metadata = {
   title: "Petroleum Industry Blogs & Insights | Geo Petroleum",
@@ -18,7 +23,12 @@ const Blogs: React.FC = () => {
     <div className='flex flex-col'>
       <ServicesSchema />
       <Header />
+      <Hero_Blogs />
+      <Intro_Blogs />
+      <Topics_Blogs />
+      <Audience_Blogs />
       <CTASection />
+      <FAQ_Blogs />
       <Footer />
     </div>
   );
