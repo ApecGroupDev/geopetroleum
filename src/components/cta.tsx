e104e29bb25689a2f1503ea6d37492100988644a
Mark Balajadia <markb.freelance@gmail.com>
2025-10-25 23:24:52 +0800

    cta design and color scheme update

--- src/components/cta.tsx
+++ src/components/cta.tsx
@@ -6,7 +6,7 @@ import ScrollToContactButton from "./CTAbutton";
 
 const CTASection = () => {
   return (
-    <section className="relative overflow-hidden bg-linear-to-r from-[#00a651] to-[#0b8c49] text-white py-20">
+    <section className="relative overflow-hidden bg-linear-to-r from-[#05c505] to-[#a5d13e] text-gray-700 py-20">
       {/* Subtle Background Glow */}
       <div className="absolute inset-0 bg-[radial-gradient(circle_at_center,white/20,transparent_70%)] opacity-20" />
 
@@ -18,28 +18,28 @@ const CTASection = () => {
           transition={{ duration: 0.6, ease: "easeOut" }}
           className="text-center lg:text-left"
         >
-          <h2 className="text-3xl sm:text-4xl lg:text-5xl font-extrabold tracking-tight leading-tight">
+          <h2 className="text-2xl sm:text-3xl lg:text-4xl text-gray-800! font-extrabold tracking-tight leading-tight">
             Let’s Build a Reliable Future Together
           </h2>
-          <p className="mt-4 text-white/90 text-base sm:text-lg max-w-lg mx-auto lg:mx-0">
-            At <span className="font-semibold text-white">Geo Petroleum</span>, we provide complete petroleum solutions —
+          <p className="mt-4 text-gray-800 text-base sm:text-lg max-w-lg mx-auto lg:mx-0">
+            At <span className="font-semibold text-gray-700">Geo Petroleum</span>, we provide complete petroleum solutions —
             from equipment and compliance to manufacturing and maintenance.
           </p>
 
           {/* Contact Info */}
-          <div className="mt-6 flex flex-col sm:flex-row sm:items-center sm:justify-center lg:justify-start gap-4 text-white/90">
+          <div className="mt-6 flex flex-col sm:flex-row sm:items-center sm:justify-center lg:justify-start gap-4 text-gray-800">
             <div className="flex items-center gap-2">
-              <Phone className="w-5 h-5 text-white" />
+              <Phone className="w-5 h-5 text-gray-700" />
               <a
                 href="tel:+8554442732"
                 className="hover:text-white transition-colors duration-200"
               >
-                855-444-2732
+                844-GEO-4040
               </a>
             </div>
             <div className="hidden sm:block w-px h-5 bg-white/40" />
             <div className="flex items-center gap-2">
-              <Mail className="w-5 h-5 text-white" />
+              <Mail className="w-5 h-5 text-gray-700" />
               <a
                 href="mailto:info@geopetroleum.com"
                 className="hover:text-white transition-colors duration-200"
