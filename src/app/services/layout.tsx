dae17c56e2703353cc1cd1d8263677e6f1aaeae0
Mark Balajadia <markb.freelance@gmail.com>
2025-10-25 23:47:56 +0800

    services main page layout naming update

--- src/app/services/layout.tsx
+++ src/app/services/layout.tsx
@@ -1,4 +1,4 @@
-export default function ServicesLayout({ children }: { children: React.ReactNode }) {
+export default function EnvironmentalRiskAssessmentLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className='w-full'>{children}</div>
   );
