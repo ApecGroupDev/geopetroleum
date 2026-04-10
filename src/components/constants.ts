3bcf85ba0df0ec89ad8c1aac3440d223be53a86a
Mark Balajadia <markb.freelance@gmail.com>
2026-04-11 01:20:18 +0800

    new component for nav links and social links

---
+++ src/components/constants.ts
@@ -0,0 +1,33 @@
+export const NAV_LINKS = [
+  { label: "Home", path: "/" },
+  { label: "Services", path: "/services" },
+  { label: "Careers", path: "/careers" },
+  { label: "Blog", path: "/blogs" },
+  { label: "Contact", path: "/contact" },
+];
+
+export const SERVICE_LINKS = [
+  {
+    label: "Environmental Risk Assessment",
+    path: "/services/environmental-risk-assessment",
+  },
+  {
+    label: "Environmental Compliance Solutions",
+    path: "/services/environmental-compliance-solutions",
+  },
+  {
+    label: "Canopy Imaging Solutions",
+    path: "/services/canopy-imaging-solutions",
+  },
+  {
+    label: "Fuel Tanks Manufacturing",
+    path: "https://www.metalproductsusa.com/",
+  },
+];
+
+export const SOCIALS = [
+  "https://www.facebook.com/",
+  "https://www.linkedin.com/",
+  "https://www.instagram.com/",
+  "https://twitter.com/",
+];
